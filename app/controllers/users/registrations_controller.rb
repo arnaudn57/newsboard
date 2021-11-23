31a75e5dc881528aac4126bb287f034e0d250704
arnaud nicastro <arnaud.nicastro@gmail.com>
2021-11-23 21:42:36 +0100

    mise a jour

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,9 +15,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # GET /resource/edit
-  def edit
-    @user = User.find(params[:id])
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
   # PUT /resource
   # def update
