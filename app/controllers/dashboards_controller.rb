817e9a9ae205884bc1fae1dae75d6bc076538cf7
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2021-11-26 00:38:43 +0100

    Carousel

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,31 +1,31 @@
 class DashboardsController < ApplicationController
   def index
-#     @user = current_user
-#     @dashboard = Dashboard.find_by(user_id: @user.id)
-#     @user_categories = @dashboard.user.user_categories
-#     @media_categories = []
-#     @user_categories.each do |user_categorie|
-#       @media_categories << Article.where(category: user_categorie.category)
-#     end
-#     if (10..20).include?(@user.available_time)
-#       @three_articles = @media_categories.each do |media_category|
-#         media_category.limit(3)
-#         raise
-#       end
-#       @medias = @three_articles.each do |article|
-#         Medium.create(mediable: article)
-#       end
-#     elsif (20..30).include?(@user.available_time)
-#       @six_articles = @media_categories.limit(6)
-#       @medias = @six_articles.each do |article|
-#         Medium.create(mediable: article)
-#       end
-#     elsif (30..40).include?(@user.available_time)
-#       @nine_articles = @media_categories.limit(9)
-#       @medias = @nine_articles.each do |article|
-#         Medium.create(mediable: article)
-#       end
-#     end
+
+    @user = current_user
+    @dashboard = Dashboard.find_by(user_id: @user.id)
+    @user_categories = @dashboard.user.user_categories
+
+
+    if (10..20).include?(@user.available_time)
+      @three_articles = Article.limit(3)
+      @medias = @three_articles.each do |article|
+        Medium.create(mediable: article)
+      end
+    elsif (20..30).include?(@user.available_time)
+      @six_articles = Article.limit(6)
+      @medias = @six_articles.each do |article|
+        Medium.create(mediable: article)
+      end
+    elsif (30..40).include?(@user.available_time)
+      @nine_articles = Article.limit(9)
+      @medias = @nine_articles.each do |article|
+        Medium.create(mediable: article)
+      end
+    end
+
+    # ---------------Carousel-----------
+    # @user_category
+
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,11 @@ class User < ApplicationRecord
   has_many :user_media_types
   has_many :user_categories
 
-  CATEGORIES = ["sport", "cinema", "tech", 'politique']
 
   def self.create_all_users_new_dashboards
     self.find_each do |user|
       new_dashboard = Dashboard.create(user: user, date: Date.today)
-      categories = CATEGORIES.sample(user.number_of_articles)
+      categories = Medium::CATEGORIES.sample(user.number_of_articles)
       categories.each do |category|
         # api
         url = "https://api.newscatcherapi.com/v2/search?q=#{category}&lang=fr"
@@ -36,7 +35,7 @@ class User < ApplicationRecord
   end
 
   def number_of_articles
-    3
+    6
     # a calculer en fonction du temps souhaite
   end
 end
