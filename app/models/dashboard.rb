d124366eab253102916eb8b4bdd55fc0e891996e
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2021-11-26 15:54:36 +0100

    Version arnaud

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,4 +1,21 @@
 class Dashboard < ApplicationRecord
   belongs_to :user
-  has_many :media
+  has_many :medium
+
+  def elements
+    medium.map(&:mediable)
+  end
+
+  def articles
+    elements.select { |element| element.is_a?(Article)}
+  end
+
+  def podcasts
+    elements.select { |element| element.is_a?(Podcast)}
+  end
+
+  def videos
+    elements.select { |element| element.is_a?(Video)}
+  end
+
 end
