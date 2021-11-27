880971bdd20ef180fe4c19ff2a4d799e4f1e1bd0
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2021-11-27 23:26:01 +0100

    Carousel Dashboard non refacto

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -8,9 +8,27 @@ class DashboardsController < ApplicationController
     #Arriver a retrouvé le dernier dashboard du user et le mettre dans @dashboard
     @dashboard = @user.dashboards.order(:created_at).last
 
-    @dashboards = @user.dashboard.order(:create_at).all
+    # @dashboards = @user.dashboard.order(:create_at).all
 
-    @articles = @dashboard.articles.select { |article| categories.include?(article.category)}
+    @articles = @dashboard.articles.select { |article| categories.include?(article.category)} #Ne sert plus
 
+    # @dashboard.articles = Articles deja trier selon l'utilisateur
+
+    # !-----------------DASHBOARD CAROUSEL----------------------
+      # ?-------------CLASS ACTIVE------------------
+
+        # @first_dashboard = @user.dashboards.order(:created_at).last
+        # @y = @user.dashboards.last.articles
+
+      # ?-----------END CLASS ACTIVE-------------------------------
+
+      # ?-----------OTHER DASHBOARDS-----------------------------
+      # @u = @user.dashboards.order(:created_at).last(7)
+      # @y = @u.clone
+      # @y.pop
+
+
+      # ?-----------END OTHERDASHBOARDS-----------------------
+    # !------------------END CAROUSEL----------------------------
   end
 end
