ae2a79b75bb7a4fb0eb8fbd7fde31f81d394a0dd
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2021-11-27 14:55:42 +0100

    versions alexis pour aller chercher les dashboards

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,6 +6,9 @@ class DashboardsController < ApplicationController
 
     #Arriver a retrouvé le dernier dashboard du user et le mettre dans @dashboard
     @dashboard = @user.dashboards.order(:created_at).last
+
+    @dashboards = @user.dashboard.order(:create_at).all
+
     @articles = @dashboard.articles.select { |article| categories.include?(article.category)}
 
   end
